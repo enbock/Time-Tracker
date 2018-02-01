6248744505b52501eb3eb10ecdf692bf8758e526
Endre Bock <dev@itbock.de>
2018-02-01 19:37:01 +0100

    #11 Implement translator.

--- src/Settings/Language/Translator.js
+++ src/Settings/Language/Translator.js
@@ -1,3 +1,6 @@
+import Axios from 'axios';
+import YAML from 'yamljs';
+
 /**
  * Language translator.
  */
@@ -11,8 +14,10 @@ class Translator {
    * @see Manager.defaultAdapter
    */
   constructor(adapter) {
-    this.adapter  = adapter;
-    this.language = '';
+    this.adapter      = adapter;
+    this.language     = '';
+    this.publicUrl    = process.env.PUBLIC_URL || '';
+    this.translations = {};
   }
 
   /**
@@ -20,6 +25,68 @@ class Translator {
    * @param language
    */
   onChange(language) {
+    if (language === this.language) {
+      return;
+    }
+    this.language = language;
+
+    const url = this.publicUrl + '/Lang/' + this.language + '/' + this.adapter.getDomain() + '.yaml';
+
+    /* eslint no-console: ["error", { allow: ["warn", "error"] }] */
+    Axios.get(url)
+      .then(response => this.onLanguageFile(response))
+      .catch(error => console.error(error));
+  }
+
+  /**
+   * Ajax handler for success loaded language file.
+   *
+   * @param response
+   */
+  onLanguageFile(response) {
+    const data        = YAML.parse(response.data);
+    this.translations = this.flatten(data);
+    this.adapter.onChange(this.language);
+  }
+
+  /**
+   * Flatten the hierarchical data to dot written flag list.
+   *
+   * @param {Object} data
+   * @param {string} parentDomain
+   *
+   * @returns {Object}
+   */
+  flatten(data, parentDomain = '') {
+    const parent = parentDomain !== '' ? parentDomain + '.' : '';
+    let flat     = {};
+
+    for (let key in data) {
+      let value  = data[key];
+      let domain = parent + key;
+      if (value instanceof Object) {
+        flat = Object.assign(flat, this.flatten(value, domain));
+      } else {
+        flat[domain] = String(value);
+      }
+    }
+
+    return flat;
+  }
+
+  /**
+   * Translate a value.
+   *
+   * @param {string} key
+   *
+   * @returns {string}
+   */
+  translate(key) {
+    if (this.translations.hasOwnProperty(key)) {
+      return this.translations[key];
+    }
+
+    return '';
   }
 }
 
---
+++ src/Settings/Language/Translator.test.js
@@ -0,0 +1,113 @@
+import {mockAxiosAction} from 'axios';
+import React from 'react';
+import Translator from './Translator';
+import YAML from 'yamljs';
+
+jest.mock('react-dom');
+
+/**
+ * Test language translator.
+ */
+describe('Language Translator', function testLanguageTranslator() {
+  /**
+   * Test loading the language yaml.
+   */
+  it('Loads the language file', function testLoadingLanguageFile() {
+    let bound     = null;
+    const promise = {
+      then:  function onThen(callback) {
+        bound = callback;
+        return promise;
+      },
+      catch: function onCatch() {
+        return promise;
+      }
+    };
+
+    mockAxiosAction(
+      'get',
+      function onRequest(url) {
+        expect(url).toBe('/Lang/foo_BAR/Test.yaml');
+
+        return promise;
+      }
+    );
+
+    const adapter  = {getDomain: () => 'Test', onChange: jest.fn()};
+    const instance = new Translator(adapter);
+    instance.onChange('');
+    expect(bound).toBe(null);
+    instance.onChange('foo_BAR');
+    bound({data: YAML.stringify({test: {foo: 'bar'}, hello: 'world'})});
+
+    expect(instance.translations['test.foo']).toBe('bar');
+    expect(instance.translations['hello']).toBe('world');
+    expect(adapter.onChange).toHaveBeenCalled();
+    expect(adapter.onChange).toHaveBeenCalledWith('foo_BAR');
+    expect(adapter.onChange).toHaveBeenCalledTimes(1);
+  });
+
+  /**
+   * Test failing of loading the language yaml.
+   */
+  it('Failed to load the language file', function testFailedLoadingLanguageFile() {
+    // Backup for this test case
+    const orgConsole = global.console;
+
+    let bound     = null;
+    const promise = {
+      then:  function onThen() {
+        return promise;
+      },
+      catch: function onCatch(callback) {
+        bound = callback;
+        return promise;
+      }
+    };
+
+    mockAxiosAction(
+      'get',
+      function onRequest(url) {
+        expect(url).toBe('/Lang/foo_BAR/Test.yaml');
+
+        return promise;
+      }
+    );
+
+    const logMock = jest.fn();
+
+    global.console = {
+      error: logMock
+    };
+
+    const adapter  = {getDomain: () => 'Test', onChange: jest.fn()};
+    const instance = new Translator(adapter);
+    instance.onChange('foo_BAR');
+    bound('error');
+
+    expect(instance.translations).toEqual({});
+    expect(adapter.onChange).not.toHaveBeenCalled();
+    expect(logMock).toHaveBeenCalledWith('error');
+    // Restore
+    global.console = orgConsole;
+  });
+
+  /**
+   * Test translation.
+   */
+  it('Translate a key', function testTranslation() {
+    const instance        = new Translator({});
+    instance.translations = {foo: 'bar'};
+
+    expect(instance.translate('foo')).toBe('bar');
+  });
+
+  /**
+   * Test fallback translation.
+   */
+  it('Translate wrong key to empty string', function testFallbackTranslation() {
+    const instance = new Translator({});
+
+    expect(instance.translate('foo')).toBe('');
+  });
+});

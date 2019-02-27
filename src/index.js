65a7842f798515f21cf8a9ec3063ec9887873a27
Endre Bock <dev@itbock.de>
2019-02-27 21:13:46 +0100

    #18 Start improving code to current style of architecture.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Application from './Application';
-import registerServiceWorker from './registerServiceWorker';
-import Axios from 'axios';
 import './bootstrap';
+import registerServiceWorker from './registerServiceWorker';
 
 registerServiceWorker();
 
-Axios.get(process.env.PUBLIC_URL + '/lib/babel.min.js')
+fetch(process.env.PUBLIC_URL + '/lib/babel.min.js')
+  .then(response => response.text())
   .then(
-    response => {
-      const babel = new Function(response.data);
+    data => {
+      // eslint-disable-next-line
+      const babel = new Function(data);
       babel.apply(global);
-      ReactDOM.render(<Application/>, document.getElementById('root'));
+
+      startApplication();
     }
   )
 ;
+
+function startApplication() {
+  // Injections
+  const router          = {/* TODO Router component */};
+  const languageManager = {/* TODO Upgrade Language Manager */};
+  const themesManager   = {/* TODO Upgrade Themes Manager */};
+
+  ReactDOM.render(
+    <Application router={router} language={languageManager} themes={themesManager}/>,
+    document.getElementById('root')
+  );
+}

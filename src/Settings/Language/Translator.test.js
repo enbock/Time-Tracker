f0478991c43fd836e6484b7411a578476ce92743
Endre Bock <dev@itbock.de>
2019-03-16 16:23:54 +0100

    #18 Correct test and reduce describe function under 50 lines.

--- src/Settings/Language/Translator.test.js
+++ src/Settings/Language/Translator.test.js
@@ -6,80 +6,79 @@ import Translator from './Translator';
 jest.mock('react-dom');
 
 describe('Language Translator', function testLanguageTranslator() {
-  let api, converter;
-
-  beforeEach(() => {
-    api = {loadLanguage: jest.fn()};
-    converter = {convert: jest.fn()};
-  });
-
-  it('Loads the language file', function testLoadingLanguageFile(done) {
-    api = {
-      loadLanguage: jest.fn().mockImplementation((
-        path => {
-          expect(path).toBe('/Lang/foo_BAR/Test.yaml');
-
-          return Promise.resolve('Yaml Text');
-        }
-      ))
-    };
-    converter = {
-      convert: jest.fn().mockImplementation((
-        text => {
-          expect(text).toBe('Yaml Text');
-
-          return Promise.resolve({test: {foo: 'bar'}, hello: 'world'});
-        }
-      ))
-    };
-    const adapter = {getDomain: () => 'Test', onChange: jest.fn()};
-    const instance = new Translator(adapter, api, converter);
-    instance.onChange('');
-    instance.onChange('foo_BAR');
-    setTimeout(
-      () => {
-
-        expect(instance.translations['test.foo']).toBe('bar');
-        expect(adapter.onChange).toHaveBeenCalledWith('foo_BAR');
-        done();
-      }
-    );
-  });
-
-  it('Failed to load the language file', function testFailedLoadingLanguageFile() {
-    api = {
-      loadLanguage: jest.fn().mockImplementation((
-        path => {
-          expect(path).toBe('/Lang/foo_BAR/Test.yaml');
-          return Promise.reject('error');
-        }
-      ))
-    };
-    const orgConsole = global.console, logMock = jest.fn();
-    global.console = {error: logMock};
-
-    const adapter = {getDomain: () => 'Test', onChange: jest.fn()};
-    const instance = new Translator(adapter, api, converter);
-    instance.onChange('foo_BAR');
-    setTimeout(
-      () => {
-        expect(instance.translations).toEqual({});
-        expect(adapter.onChange).not.toHaveBeenCalled();
-        expect(logMock).toHaveBeenCalledWith('error');
-        global.console = orgConsole;
-        done();
-      }
-    );
-  });
-
+  it('Loads the language file', testLoadingLanguageFile);
+  it('Failed to load the language file', testFailedLoadingLanguageFile);
   it('Translate a key', function testTranslation() {
+    const api = {loadLanguage: jest.fn()};
+    const converter = {convert: jest.fn()};
     const instance = new Translator({}, api, converter);
     instance.translations = {foo: 'bar'};
     expect(instance.translate('foo')).toBe('bar');
   });
-
   it('Translate wrong key to empty string', function testFallbackTranslation() {
+    const api = {loadLanguage: jest.fn()};
+    const converter = {convert: jest.fn()};
     const instance = new Translator({}, api, converter);
     expect(instance.translate('foo')).toBe('');
   });
 });
+
+function testLoadingLanguageFile(done) {
+  const api = {
+    loadLanguage: jest.fn().mockImplementation((
+      path => {
+        expect(path).toBe('/Lang/foo_BAR/Test.yaml');
+
+        return Promise.resolve('Yaml Text');
+      }
+    ))
+  };
+  const converter = {
+    convert: jest.fn().mockImplementation((
+      text => {
+        expect(text).toBe('Yaml Text');
+
+        return Promise.resolve({test: {foo: 'bar'}, hello: 'world'});
+      }
+    ))
+  };
+  const adapter = {getDomain: () => 'Test', onChange: jest.fn()};
+  const instance = new Translator(adapter, api, converter);
+  instance.onChange('');
+  instance.onChange('foo_BAR');
+  setTimeout(
+    () => {
+
+      expect(instance.translations['test.foo']).toBe('bar');
+      expect(adapter.onChange).toHaveBeenCalledWith('foo_BAR');
+      done();
+    }
+  );
+}
+
+function testFailedLoadingLanguageFile() {
+  const api = {
+    loadLanguage: jest.fn().mockImplementation((
+      path => {
+        expect(path).toBe('/Lang/foo_BAR/Test.yaml');
+        return Promise.reject('error');
+      }
+    ))
+  };
+  const converter = {convert: jest.fn()};
+  const orgConsole = global.console, logMock = jest.fn();
+  global.console = {error: logMock};
+
+  const adapter = {getDomain: () => 'Test', onChange: jest.fn()};
+  const instance = new Translator(adapter, api, converter);
+  instance.onChange('foo_BAR');
+  setTimeout(
+    () => {
+      expect(instance.translations).toEqual({});
+      expect(adapter.onChange).not.toHaveBeenCalled();
+      expect(logMock).toHaveBeenCalledWith('error');
+      global.console = orgConsole;
+      done();
+    }
+  );
+}

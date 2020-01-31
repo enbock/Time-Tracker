b12ad10fd7db10f1fe2bf8f14c5aab15bb5a2808
[Circle CI] Endre Bock <circle-ci+dev@itbock.de>
2020-01-31 20:18:44 +0000

    auto commit [ci skip]

--- Language/Loader/Ajax.js
+++ Language/Loader/Ajax.js
@@ -1,6 +1,6 @@
 export default class Ajax {
     async loadLanguage(languageCode) {
-        const response = await fetch('../I18n/' + languageCode + '.json');
+        const response = await fetch('./I18n/' + languageCode + '.json');
         return response.json();
     }
 }

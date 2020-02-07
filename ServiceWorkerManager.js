c55db858c15167cfa4d6477671d534c2109504ea
Endre Bock <dev@itbock.de>
2020-02-07 22:23:08 +0100

    'auto

--- ServiceWorkerManager.js
+++ ServiceWorkerManager.js
@@ -1,5 +1,6 @@
-const version = '1581110118';
+const version = '1581110460';
 const fileList = ["./Application/Action.js","./Application/Container.js","./Application/ModuleLoader.js","./Application/View/Application/Model.js","./Application/View/Application/Presenter.js","./Application/View/Application.js","./Application/View/Page/Model.js","./Application/View/Page/Presenter.js","./Application/View/Page.js","./Application/View/SideMenu/Model.js","./Application/View/SideMenu/Presenter.js","./Application/View/SideMenu.js","./Application/View/TopBar/Model.js","./Application/View/TopBar/Presenter.js","./Application/View/TopBar.js","./Application.js","./favicon-128.png","./favicon-196x196.png","./favicon-196x196.webp","./favicon-512x512.png","./favicon-512x512.webp","./favicon.ico","./Font/2fcrYFNaTjcS6g4U3t-Y5ZjZjT5FdEJ140U2DJYC3mY.woff2","./Font/CWB0XYA8bzo0kSThX0UTuA.woff2","./Font/MaterialIcons-Regular.ttf","./Font/MaterialIcons-Regular.woff","./Font/MaterialIcons-Regular.woff2","./Font/RxZJdnzeo3R5zSexge8UUVtXRa8TVwTICgirnJhmVJw.woff2","./HelloWorld.js","./I18n/de-de.json","./I18n/en-us.json","./index.html","./index.js","./Language/ChangeLanguageSetup.js","./Language/Container.js","./Language/ILoader.js","./Language/Loader/Ajax.js","./Language/Manager.js","./Language/Translator/Factory.js","./Language/Translator.js","./Libraries/material-components-web.min.js","./Libraries/react-dom.min.js","./Libraries/react.min.js","./manifest.json","./Observer/ListenerAdapter.js","./Observer/Observer.js","./Style/Application.css","./Style/material-components-web.icons.css","./Style/material-components-web.min.css","./Style/Style.js","./Style/Themes/Codefrog.css","./Style/Themes/Google.css"];
+"use strict";
 // https://developers.google.com/web/fundamentals/primers/service-workers
 class ServiceWorkerManager {
     constructor(worker, caches, version, fileList) {
@@ -53,4 +54,4 @@ class ServiceWorkerManager {
         return response;
     }
 }
-export default new ServiceWorkerManager(this, caches, version, fileList);
+new ServiceWorkerManager(this, caches, version, fileList);

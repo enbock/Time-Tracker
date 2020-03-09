6a59fb39ccc8de860f324bcbe2bbde615c719431
[Circle CI] Endre Bock <circle-ci+dev@itbock.de>
2020-03-09 23:26:03 +0000

    auto commit [ci skip]

--- Application/Application.js
+++ Application/Application.js
@@ -10,30 +10,13 @@ export default class Application extends React.Component {
             loadedPage: null,
             shownPage: ''
         };
-        this.adapter = Container.applicationAction.adapter;
+        this.adapter = Container.applicationActionAdapter;
         this.moduleLoader = Container.moduleLoader;
         Container.language.adapter.addListener(this.onLanguageLoaded.bind(this));
         Container.moduleStateAdapter.addListener(this.onModuleLoaded.bind(this));
-        Container.router.adapter.addListener(this.adapter.onPageChanged);
         Container.menuOpenStateAdapter.onChange = this.onMenuChange.bind(this);
     }
     componentDidMount() {
-        Container.language.changeLanguageSetup.interact({ languageCode: 'de-de' }, {}).then();
-        const homePage = {
-            depth: 0,
-            name: 'home',
-            url: './',
-            module: './HelloWorld'
-        };
-        const settingsPage = {
-            depth: 1,
-            name: 'settings',
-            url: './settings/',
-            module: './Settings/Settings'
-        };
-        Container.router.registry.registerPage(homePage);
-        Container.router.registry.registerPage(settingsPage);
-        Container.router.observer.value = homePage;
     }
     onLanguageLoaded(oldValue, newValue) {
         this.setState({ loadedLanguage: newValue.languageCode });
--- Application/Container.js
+++ Application/Container.js
@@ -19,11 +19,32 @@ class Container {
         this.moduleLoader = new ModuleLoader('../', this.moduleState);
         this.applicationAction =
             new Action(this.menuOpenState, this.router.router, this.router.registry, this.moduleLoader);
+        this.applicationActionAdapter = this.applicationAction.adapter;
         this.topAppBarPresenter = new TopBarPresenter(this.language.observer);
         this.sideMenuPresenter =
             new SideMenuPresenter(this.menuOpenState, this.language.observer, this.router.observer, this.router.registry);
         this.pagePresenter = new PagePresenter(this.moduleState);
         this.applicationPresenter = new ApplicationPresenter(this.language.observer, this.topAppBarPresenter, this.sideMenuPresenter, this.pagePresenter);
+        this.setupDefaults();
+    }
+    setupDefaults() {
+        this.router.adapter.addListener(this.applicationActionAdapter.onPageChanged);
+        this.language.changeLanguageSetup.interact({ languageCode: 'de-de' }, {}).then();
+        const homePage = {
+            depth: 0,
+            name: 'home',
+            url: './',
+            module: './HelloWorld'
+        };
+        const settingsPage = {
+            depth: 1,
+            name: 'settings',
+            url: './settings/',
+            module: './Settings/Settings'
+        };
+        this.router.registry.registerPage(homePage);
+        this.router.registry.registerPage(settingsPage);
+        this.router.observer.value = homePage;
     }
 }
 export default new Container();
